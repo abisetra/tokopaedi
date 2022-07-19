7462fe877b73dce5bed3b037fb6288d3f2dc0c86
abisetra <abiyyusetrayana63@gmail.com>
2022-07-19 20:35:42 +0700

    rename project name to warung-online

--- warung/urls.py
+++ warung/urls.py
@@ -1,4 +1,4 @@
-"""tokopaedi URL Configuration
+"""warung-online URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
